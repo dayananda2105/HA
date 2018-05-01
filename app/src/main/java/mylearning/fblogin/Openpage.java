4b96511576b090ecd0a27dae2ef8b77589c35b55
dayanandamahankali <dayanandamahankali@gmail.com>
2018-05-01 18:58:18 +0530

    login&res

--- app/src/main/java/mylearning/fblogin/Openpage.java
+++ app/src/main/java/mylearning/fblogin/Openpage.java
@@ -8,7 +8,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -16,7 +16,6 @@ import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 
 public class Openpage extends AppCompatActivity {
     EditText text1;
@@ -53,5 +52,56 @@ public class Openpage extends AppCompatActivity {
                         startActivity(mov);
             }
         });
+    }
+
+    public static class Loginapp extends AppCompatActivity {
+        Button log,reg;
+        EditText un,ps,mail;
+        LinearLayout lp;
+        FirebaseAuth firebaseAuth;
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_loginapp);
+            firebaseAuth=FirebaseAuth.getInstance();
+            log=(Button)findViewById(R.id.log);
+            reg=(Button)findViewById(R.id.reg);
+            un=(EditText)findViewById(R.id.un);
+            ps=(EditText)findViewById(R.id.ps);
+            lp=(LinearLayout)findViewById(R.id.lp);
+            log.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if(un.getText().toString().equals("daya")&&ps.getText().toString().equals("2105"))
+                    {
+                        Intent go=new Intent(Loginapp.this,Openpage.class);
+                        startActivity(go);
+                        Toast.makeText(Loginapp.this, "u r valid", Toast.LENGTH_SHORT).show();
+                    }
+                    else {
+                        firebaseAuth.signInWithEmailAndPassword(un.getText().toString(), ps.getText().toString()).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+                                if (task.isSuccessful()) {
+                                    Toast.makeText(Loginapp.this, "login succesful", Toast.LENGTH_SHORT).show();
+                                    startActivity(new Intent(Loginapp.this, Openpage.class));
+                                } else {
+                                    Toast.makeText(Loginapp.this, "login failed", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
+                    }
+                }
+            });
+            reg.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent gos=new Intent(Loginapp.this,Register.class);
+                    startActivity(gos);
+                }
+            });
+
+        }
     }
 }
